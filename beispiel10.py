c2148edfcda0e35e7e53973b74705401813f2c18
Maschi62 <125575193+Maschi62@users.noreply.github.com>
2026-09-22 21:42:20 +0200

    Erstelle ein Python-Beispiel zum Addieren zweier Zahlen

--- beispiel10.py
+++ beispiel10.py
@@ -1,1 +1,7 @@
+# Zwei Zahlen abfragen und addieren
+zahl1 = float(input("Gib die erste Zahl ein: "))
+zahl2 = float(input("Gib die zweite Zahl ein: "))
 
+summe = zahl1 + zahl2
+
+print("Die Summe ist:", summe)
